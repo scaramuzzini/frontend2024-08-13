d4c4cebde9eb3f014e71398729f9546bd6113884
Mario Torres <scaramuzzini@gmail.com>
2024-08-13 10:47:46 -0400

    Starlink name list page 1

--- src/StarlinkList.js
+++ src/StarlinkList.js
@@ -1,7 +1,36 @@
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function StarlinkList() {
-    return <h4>Meu componente starlink</h4>
+
+    const [starlinks, setStarlinks] = useState([]);
+
+    const fetchStartlinks = async () => {
+        const response = await axios.post('https://api.spacexdata.com/v4/starlink/query', {
+            "query": {},
+            "options": { limit: 10 }
+        });
+        console.log(response.data);
+        setStarlinks(response.data.docs);
+    }
+
+    useEffect(() => {
+        fetchStartlinks();
+    }, []);
+
+
+    return (
+        <>
+            <h4>Meu componente starlink</h4>
+            <ul>
+                {starlinks.map((sat) => (
+                        <li key={sat.id}> {sat.spaceTrack.OBJECT_NAME}</li>
+                    )
+                )
+                }
+            </ul>
+        </>
+    );
 }
 
 export default StarlinkList;
